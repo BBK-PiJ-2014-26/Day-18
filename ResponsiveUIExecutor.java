f25ad29f42800ffeddcf8c3512a5f6976d342cf9
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-02-23 23:51:49 +0000

    3.1 sorta solved

--- ResponsiveUIExecutor.java
+++ ResponsiveUIExecutor.java
@@ -1,4 +1,6 @@
 import java.util.concurrent.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Answers Q3 Day-18
@@ -6,21 +8,59 @@ import java.util.concurrent.*;
  */
 
 public class ResponsiveUIExecutor implements Executor {
-	private final ArrayBlockingQueue<Runnable> jobQueue = new ArrayBlockingQueue(10);
-	private final ExecutorService myThreadPool = newFixedThreadPool(4);
+	private final Queue<Runnable> jobQueue;
+	private final Thread t1 = null;
+	private final Thread t2 = null;
 
+	public ResponsiveUIExecutor() {
+		this.jobQueue = new LinkedList<Runnable>();
+	}
+
+	/**
+	 * Checks if threads are in use, if not, it adds the task to the queue.
+	 */
 	@Override
 	public void execute(Runnable r) {
+		synchronized (t1) {
+			if (t1 == null) {
+				runJob(r, this.t1);
+			} else {
+				synchronized (t2) {
+					if (t2 == null) {
+						runJob(r, this.t2);
+					} else {
+						addToQueue(r);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Runs the current task. Once the taks is complete, it runs the next task in the queue.
+	 * Otherwise, the thread is reset to null.
+	 */
+	public void runJob(Runnable r, Thread t) {
+		t.start();
+		if (!jobQueue.isEmpty()) {
+			runJob(removeFromQueue(), t);
+		} else {
+			t = null;
+		}
+	}
+
+	/**
+	 * Adds a runnable object to the jobQueue.
+	 */
+	public synchronized void addToQueue(Runnable r) {
 		jobQueue.add(r);
 	}
 
-	public void addToQueue(Runnable r) {
-
-
-	public synchronized void runJob() {
-		wait();
-		Object o = take();
-		Thread
-
+	/**
+	 * Removes the head of the queue.
+	 */
+	public synchronized Runnable removeFromQueue() {
+		Runnable result = jobQueue.remove();
+		return result;
+	}
 }
-
